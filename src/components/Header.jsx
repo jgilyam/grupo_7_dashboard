4d0f0e9b219aa5e498d6eafcd899c967a8d82b93
jgilyam <jgilyam@folcode.com>
2022-07-18 01:45:39 -0300

    maquetacion de header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/css/header.css";
+function Header() {
+  return (
+    <div>
+      <div className="container-header">
+        <header></header>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
