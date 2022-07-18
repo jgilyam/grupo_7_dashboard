b36edd7c767b29c5b2b4fd8301d42aec5dfeedc0
jgilyam <jgilyam@folcode.com>
2022-07-18 00:06:32 -0300

    NotFopunt  component added

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function NotFound() {
+  return (
+    <React.Fragment>
+      {/*<!-- Content Wrapper -->*/}
+
+      {/*<!-- Main Content -->*/}
+      <div id="content">
+        <p>404sssssssssss</p>
+      </div>
+    </React.Fragment>
+  );
+}
+export default NotFound;
